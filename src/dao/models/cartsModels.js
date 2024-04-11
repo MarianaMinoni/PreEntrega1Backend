af695ccb2105fc6e8ed1c982a1f0506bdd598a0d
MarianaMinoni <123034714+MarianaMinoni@users.noreply.github.com>
2024-04-11 07:11:30 -0300

    cartModel

--- src/dao/models/cartsModels.js
+++ src/dao/models/cartsModels.js
@@ -1,2 +1,28 @@
 import mongoose from "mongoose";
 
+//llamo a la coleccion que quiero usar
+const cartCollection = "carts"
+
+
+//creo el modelo de datos
+const cartSchema =  new mongoose.Schema({
+    products : {
+        type: [
+            {
+                product: {
+                    type : mongoose.Schema.ObjectId,
+                    ref: "products"
+
+                },
+                quantity : {
+                    type: Number,
+                    default: 1
+                }
+            }
+        ],
+        default: []
+    }
+})
+
+// compilo y exporto el modelo
+export const cartmodel = mongoose.model(cartCollection, cartSchema)
